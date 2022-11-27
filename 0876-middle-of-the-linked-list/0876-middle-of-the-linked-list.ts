a0964273d9941de4ef0cd9703f80126848b1242a
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2022-11-27 22:22:46 +0300

    Time: 101 ms (54.34%), Space: 43.2 MB (44.34%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let [fast, slow] = [head, head]
+
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    return slow
+};
