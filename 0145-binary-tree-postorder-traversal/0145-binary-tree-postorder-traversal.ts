d3b7534766e235b803e7fd46743d89687b52fa40
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2022-12-10 04:16:42 +0300

    Time: 69 ms (90.38%), Space: 43 MB (98.56%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return []
+
+    const stack = [root]
+    const output = []
+
+    while (stack.length) {
+        const node = stack.pop()
+
+        if (!node) continue
+
+        output.push(node.val)
+
+        stack.push(node.left)
+        stack.push(node.right)
+    }
+
+    return output.reverse()
+};
