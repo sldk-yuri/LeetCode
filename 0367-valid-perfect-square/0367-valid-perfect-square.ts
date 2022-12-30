48f0ee093d4152cf74e83ba666f8f78819ced4f4
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2022-12-30 12:57:23 +0300

    Time: 67 ms (87.88%), Space: 43.1 MB (42.42%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.ts
@@ -0,0 +1,13 @@
+function isPerfectSquare(num: number): boolean {
+    let left = 1, right = num;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (mid * mid === num) return true;
+        else if (mid * mid > num) right = mid - 1;
+        else left = mid + 1;
+    }
+
+    return false;
+};
